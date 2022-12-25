291384cbe02e3d748e3f42cd92f6e1c93081723b
Ilya Kuligin <kuligin-ilya@mail.ru>
2022-12-26 01:03:25 +0300

    readme commit

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Привет мир");
         System.out.println("test");
         System.out.println("test 2");
         System.out.println("git test");
